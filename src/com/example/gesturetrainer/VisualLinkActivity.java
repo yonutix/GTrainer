2cf55f4103cda8bf6cb6befd251a5da8c2010134
Mihai Ionut-Cosmin <yonutix@yahoo.com>
2014-08-17 23:05:18 +0300

    changed package name
    Signed-off-by: Mihai Ionut-Cosmin <yonutix@yahoo.com>

--- src/com/example/gesturetrainer/VisualLinkActivity.java
+++ src/com/example/gesturetrainer/VisualLinkActivity.java
@@ -1,7 +1,7 @@
 package com.example.gesturetrainer;
 
-import android.os.Bundle;
 import android.app.Activity;
+import android.os.Bundle;
 import android.view.Menu;
 
 public class VisualLinkActivity extends Activity {
